2362d5786715a990f2bdae59fa6ee7a62825971f
Ramon Sales <ramon.silvasales@gmail.com>
2017-08-27 20:56:15 -0300

    I have no idea what i'm doing

--- djangobasico/settings.py
+++ djangobasico/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'djangobasico/static'),
 )
